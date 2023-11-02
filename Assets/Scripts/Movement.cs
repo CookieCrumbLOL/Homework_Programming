c9740681ecac67aac4da842d944b7c0d7c8f982a
CookieCrumbLOL <146147752+CookieCrumbLOL@users.noreply.github.com>
2023-11-01 22:59:39 -0400

    Frankie
    programming homework

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,5 +2,23 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    private Rigidbody2D _rigidbody2D;
+    private float _xVelocity;
+    private float _yVelocity;
+    public float speed = 3;
 
+
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+
+   void Update()
+    {
+
+        _xVelocity = Input.GetAxis("horizontal") * speed;
+        _yVelocity = Input.GetAxis("vertical") * speed;
+        _rigidbody2D.velocity = new Vector2(_xVelocity, _yVelocity);
+    }
 }
--- Assets/Scripts/PlayerDie.cs
+++ Assets/Scripts/PlayerDie.cs
@@ -3,4 +3,16 @@ using UnityEngine;
 public class PlayerDie : MonoBehaviour
 {
 
+    public GameObject canvas;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            canvas.gameObject.SetActive(true);
+
+            gameObject.SetActive(false);
+        }
+    }
 }
